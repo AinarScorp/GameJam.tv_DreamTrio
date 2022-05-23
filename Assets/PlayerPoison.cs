f35ce01422fd14871aa87bab3adbeccf93dcafeb
AinarScorp <Einarhallik@gmail.com>
2022-05-23 11:37:34 +0200

    poison now

--- Assets/PlayerPoison.cs
+++ Assets/PlayerPoison.cs
@@ -2,14 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerHealth))]
 public class PlayerPoison : MonoBehaviour
 {
-    EnemyHealth enemyHealth;
+    [SerializeField] [Range(1,120f)] float damageInterval = 2f;
+    [SerializeField] int poisonDamage = 1;
+
+    PlayerHealth player;
 
     private void Awake()
     {
-        enemyHealth = GetComponent<EnemyHealth>();
+        player = GetComponent<PlayerHealth>();
     }
 
+    private void Start()
+    {
+        StartCoroutine(PoisonPlayer());
+    }
+    IEnumerator PoisonPlayer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(damageInterval);
+
+            if (player.IsAlive)
+                player.ReceiveDamage(poisonDamage);
+
+
+        }
+    }
 
 }
