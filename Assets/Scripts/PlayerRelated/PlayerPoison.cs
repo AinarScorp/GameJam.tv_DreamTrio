748f5f95dcab3cfd16356760c33d3f31817f4855
Jonicles <105931995+Jonicles@users.noreply.github.com>
2022-05-25 13:55:30 +0200

    aa

--- Assets/Scripts/PlayerRelated/PlayerPoison.cs
+++ Assets/Scripts/PlayerRelated/PlayerPoison.cs
@@ -5,13 +5,10 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerHealth))]
 public class PlayerPoison : MonoBehaviour
 {
-<<<<<<< HEAD
     [SerializeField] FlashEffect flashScript;
     [SerializeField] ParticleSystem poisonParticle;
     [SerializeField] [Range(1, 120f)] float damageInterval = 2f;
-=======
-    [SerializeField] [Range(0.01f,100f)] float damageInterval = 1f;
->>>>>>> 9017dc5ed367fb11137d61d0ce20153476c86315
+
     [SerializeField] int poisonDamage = 1;
 
     PlayerHealth player;
